cb5ab970d4bfc76bb8ba51fd38c515d5bb578c1f
whitejamer <mihailkuzin.job@yandex.ru>
2020-04-01 14:13:12 +0300

    Make confirm-plugin extends Modal-functional

---
+++ js/plugins/confirm.js
@@ -0,0 +1,27 @@
+displayJS.confirm = function(options){
+	return new Promise((resolve, reject) => {
+		const modal = displayJS.modal({
+			title: options.title,
+			content: options.content,
+			closable: false,
+			onClose(){
+				setTimeout(() => {
+					modal.destroy()
+				}, 100)
+			},
+			footerButtons: [
+				{text: 'Удалить', type: 'danger', handler() {
+					modal.close()
+					resolve()
+				}},
+				{text: 'Отмена', type: 'secondary', handler() {
+					modal.close()
+					reject()
+				}},
+			],
+		})
+		setTimeout(() => {
+			modal.open()
+		}, 100)
+	})
+} 
